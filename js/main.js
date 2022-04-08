5619fde17150400ee89ad8f19f21ca3c63f96131
Skretzo <53493631+Skretzo@users.noreply.github.com>
2022-04-08 20:10:29 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -40,4 +40,58 @@ function getTitle()
     return toTitleCase(getFolder()).replace(/-/g, " ");
 }
 
-document.title = getTitle();
+function setMetaTags()
+{
+    // <meta property = "og:type" content = "video.other">
+    // <meta property = "og:url" content = getHref()>
+    // <meta property = "og:video" content = getHref() + "video.mp4">
+    // <meta property = "og:video:type" content = "video/mp4">
+    // <meta property = "og:image" content = getHref() + "image.png">
+    // <meta property = "og:title" content = getTitle()>
+    // <meta property = "og:description" content = "Description text">
+    // <meta property = "og:site_name" content = "Videos">
+    // <meta name = "twitter:card" content = "player">
+    // <meta name = "twitter:site" content = "@skretzo">
+    // <meta name = "twitter:image" content = getHref() + "image.png">
+    // <meta name = "twitter:player" content = getHref()>
+    // <meta name = "twitter:player:width" content = "640">
+    // <meta name = "twitter:player:height" content = "360">
+
+    var NAMES = ["twitter:card", "twitter:site", "twitter:image",
+                 "twitter:player", "twitter:player:width", "twitter:player:height"];
+    var CONTENTS = ["player", "@skretzo", getHref() + "image.png", getHref(), "640", "360"];
+
+    var h = document.getElementsByTagName("head")[0];
+    for (var i = 0; i < NAMES.length; i++)
+    {
+        var NAME = NAMES[i];
+        var CONTENT = CONTENTS[i];
+        var m = document.createElement("meta");
+        m.name = NAME;
+        m.content = CONTENT;
+        h.appendChild(m);
+    }
+}
+
+function setTitle()
+{
+    document.title = getTitle();
+}
+
+function setSubtitle()
+{
+    document.getElementById("subtitle").innerHTML = getTitle();
+}
+
+function loadHead()
+{
+    setMetaTags();
+    setTitle();
+}
+
+function load()
+{
+    setSubtitle();
+}
+
+loadHead();
